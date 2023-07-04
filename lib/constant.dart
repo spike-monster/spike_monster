2ab9a4ab3386ae38efbef7993e5a593391635b5f
Lichyo <lichyo003@gmail.com>
2023-07-04 11:38:14 +0800

    create titleTextStyle

---
+++ lib/constant.dart
@@ -0,0 +1,10 @@
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+
+final kTitleTextStyle = GoogleFonts.getFont(
+  'Ubuntu',
+  fontSize: 45.0,
+  fontWeight: FontWeight.w500,
+);
+
+
--- lib/view/welcome_page.dart
+++ lib/view/welcome_page.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:spike_monster/constant.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            colors: [
+              Colors.orangeAccent.shade700,
+              Colors.orangeAccent.shade200,
+            ],
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, top: 80.0),
+              child: Text(
+                'Spike Monster',
+                style: kTitleTextStyle,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
