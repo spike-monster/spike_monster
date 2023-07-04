efeb47e1140cee4bd8dd688bbba283f02e12a69b
Lichyo <lichyo003@gmail.com>
2023-07-04 12:44:36 +0800

    update crow image

--- lib/view/welcome_page.dart
+++ lib/view/welcome_page.dart
@@ -1,8 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:spike_monster/constant.dart';
 
-class WelcomePage extends StatelessWidget {
+class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> with SingleTickerProviderStateMixin{
+  late AnimationController _animationController;
+  late Image _crowImageSprite1;
+  late Image _crowImageSprite2;
+  late Image _crow;
+
+  void loadCrowImage() {
+    _crowImageSprite1 = Image.asset('images/crow_sprite1.png');
+    _crowImageSprite2 = Image.asset('images/crow_sprite2.png');
+  }
+
+  void crowFlying() {
+    if(_animationController.value %5 == 0) {
+      setState(() {
+        _crow = _crowImageSprite1;
+      });
+    } else {
+      setState(() {
+        _crow = _crowImageSprite2;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadCrowImage();
+    _crow = _crowImageSprite1;
+    _animationController = AnimationController(
+      upperBound: 100,
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    );
+    // crowFlying();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +75,7 @@ class WelcomePage extends StatelessWidget {
                 style: kSubtitleTextStyle,
               ),
             ),
-
+            Expanded(child: _crow),
           ],
         ),
       ),
