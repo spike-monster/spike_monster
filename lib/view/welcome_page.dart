74410e2e6f4c806d4f4222fc2ebc2bc76dce3bd3
Lichyo <lichyo003@gmail.com>
2023-07-05 22:24:04 +0800

    upload volleyball image

--- lib/view/welcome_page.dart
+++ lib/view/welcome_page.dart
@@ -12,53 +12,11 @@ class WelcomePage extends StatefulWidget {
 class _WelcomePageState extends State<WelcomePage>
     with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
-  late Image _crowImageSprite1;
-  late Image _crowImageSprite2;
-  late Image _crow;
-  int crowFlyingHeight = 0;
-  int crowFlyingLength = 0;
 
-  void loadCrowImage() {
-    _crowImageSprite1 = Image.asset('images/crow_sprite1.png');
-    _crowImageSprite2 = Image.asset('images/crow_sprite2.png');
-  }
-
-  void crowFlying() {
-    Timer.periodic(const Duration(milliseconds: 300), (timer) {
-      if (_crow == _crowImageSprite1) {
-        _crow = _crowImageSprite2;
-        crowFlyingLength += 10;
-        crowFlyingHeight += 10;
-      } else {
-        _crow = _crowImageSprite1;
-        crowFlyingHeight -= 10;
-      }
-    });
-  }
-
-  double resetCrowFlyingLength() {
-    setState(() {
-      crowFlyingLength = 0;
-    });
-    return 0;
-  }
 
   @override
   void initState() {
     super.initState();
-    loadCrowImage();
-    _crow = _crowImageSprite2;
-    _animationController = AnimationController(
-      lowerBound: 0,
-      upperBound: 400,
-      duration: const Duration(seconds: 20),
-      vsync: this,
-    );
-    _animationController.forward();
-    _animationController.addListener(() {
-      setState(() {});
-    });
-    crowFlying();
   }
 
   @override
@@ -94,18 +52,7 @@ class _WelcomePageState extends State<WelcomePage>
                   style: kSubtitleTextStyle,
                 ),
               ),
-              Container(
-                height: 150.0,
-                width: 200,
-                alignment: Alignment.topRight,
-                padding: EdgeInsets.only(
-                  right: (crowFlyingLength + 10 > screenWidth)
-                      ? resetCrowFlyingLength()
-                      : crowFlyingLength.toDouble(),
-                  top: crowFlyingHeight.toDouble() + 20,
-                ),
-                child: _crow,
-              ),
+
             ],
           ),
         ),
