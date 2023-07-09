bacb2ba7373a88ca2f49e84198b56450fad28173
Lichyo <lichyo003@gmail.com>
2023-07-10 00:59:26 +0800

    _auth API : - createAnAccountWithEmail - loginWithEmail - getAccountDetail return Account Model

--- lib/model/account.dart
+++ lib/model/account.dart
@@ -1,15 +1,15 @@
 class Account {
   String name;
   String email;
+  String password;
   int record;
   String? id;
 
   Account({
     required this.name,
     required this.email,
+    required this.password,
     this.record = 0,
     this.id,
   });
-
-
 }
