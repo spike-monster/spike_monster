71e803b5024e7f54306343421c250053f26e0b1a
Lichyo <lichyo003@gmail.com>
2023-08-02 13:48:53 +0800

    fix conflic

--- lib/view/join_page.dart
+++ lib/view/join_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spike_monster/view/profile_page.dart';
 
 class JoinPage extends StatefulWidget {
   const JoinPage({super.key});
@@ -14,48 +15,104 @@ class _JoinPageState extends State<JoinPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 30.0),
         color: Colors.orangeAccent.shade200,
-        child: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const Text(
-                  'Join Game',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                TextField(
-                  onChanged: (value) {
-                    playerID = value;
-                  },
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    labelText: 'Enter Player ID',
-                    labelStyle: TextStyle(
-                      fontSize: 30,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            MaterialButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const ProfilePage()));
+              },
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const Text(
+                      'Join Game',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
                     ),
-                  ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                MaterialButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Join',
-                    style: TextStyle(
-                      fontSize: 30,
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 80.0, vertical: 50.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          const Text(
+                            'Join Game',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 30,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                          TextField(
+                            onChanged: (value) {
+                              playerID = value;
+                            },
+                            obscureText: true,
+                            decoration: const InputDecoration(
+                                labelText: 'Enter Player ID',
+                                labelStyle: TextStyle(
+                                  fontSize: 25,
+                                )),
+                          ),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                          MaterialButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Join',
+                              style: TextStyle(
+                                fontSize: 25,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    TextField(
+                      onChanged: (value) {
+                        playerID = value;
+                      },
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        labelText: 'Enter Player ID',
+                        labelStyle: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    MaterialButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Join',
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ]),
+              ),
+            ),
+          ],
         ),
       ),
     );
