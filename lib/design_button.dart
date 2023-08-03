57b7e1fbbc3c7adc1401279a382c3e85d0df9018
Lichyo <lichyo003@gmail.com>
2023-08-03 23:31:04 +0800

    button ver 0.0.0

--- lib/design_button.dart
+++ lib/design_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'constants.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,32 +11,47 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: GameButton(name: Button.spike, image: Image.asset('name'), onPressed: onPressed),
+          child: GameButton(
+            name: Button.spike,
+            image: const AssetImage('images/button_icon/spike.png'),
+            onPressed: () {},
+          ),
         ),
       ),
     );
   }
 }
 
-
-
 class GameButton extends StatelessWidget {
-  const GameButton({
+  GameButton({
     super.key,
     required this.name,
     required this.image,
     required this.onPressed,
-  });
+    this.size,
+  }) {
+    biggerRadius = size != null ? size! + 3 : null;
+  }
 
+  final double? size;
   final Button name;
-  final Image image;
+  final AssetImage image;
   final VoidCallback onPressed;
+  double? biggerRadius;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
+    return MaterialButton(
+      elevation: 5,
       onPressed: onPressed,
-      child: image,
+      child: CircleAvatar(
+        backgroundColor: Colors.brown.shade300,
+        radius: biggerRadius ?? 73,
+        child: CircleAvatar(
+          radius: size ?? 70,
+          backgroundImage: image,
+        ),
+      ),
     );
   }
 }
